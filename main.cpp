89ee40b1527011e57c4af46cc0285a237b3e8b10
Chandika Nettasinghe <93026500+Nettasinghe@users.noreply.github.com>
2022-01-01 09:47:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 // Fair usage policies limit the usage of one user over another at peak times so that the performance 
 // is not affected for the rest of the group. Fair User Policy Engine implements this logic by 
 // fetching the data usages of data service subscribers from the database at real time and facilitate 
-// downgrading of the bandwith of the subscribers who has exceeded the configured threshold levels.
+// downgrading of the bandwidth of the subscribers who has exceeded the configured threshold levels.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
